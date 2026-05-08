6ed5ae33262d5c0cad3193fd3f2a2190c98d3794
Shlokuchavan <gajvakraresedency@gmail.com>
2026-05-08 13:03:09 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,42 @@
 package com.example;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+
+    public void testAddition() {
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int expected = 30;
+
+        int actual = num1 + num2;
+
+        assertEquals(expected, actual);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testAdditionWithZero() {
+
+        int num1 = 15;
+        int num2 = 0;
+
+        int expected = 15;
+
+        int actual = num1 + num2;
+
+        assertEquals(expected, actual);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testNegativeNumbers() {
+
+        int num1 = -5;
+        int num2 = -10;
+
+        int expected = -15;
+
+        int actual = num1 + num2;
+
+        assertEquals(expected, actual);
     }
 }
