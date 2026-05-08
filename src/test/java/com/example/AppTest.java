b3163f476109880b4893a1855c360941206579c6
Shlokuchavan <gajvakraresedency@gmail.com>
2026-05-08 13:29:15 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -9,7 +9,7 @@ public class AppTest extends TestCase {
         int num1 = 10;
         int num2 = 20;
 
-        int expected = 30;
+        int expected = 25; // Wrong expected value
 
         int actual = num1 + num2;
 
@@ -21,7 +21,7 @@ public class AppTest extends TestCase {
         int num1 = 15;
         int num2 = 0;
 
-        int expected = 15;
+        int expected = 10; // Wrong expected value
 
         int actual = num1 + num2;
 
@@ -33,7 +33,7 @@ public class AppTest extends TestCase {
         int num1 = -5;
         int num2 = -10;
 
-        int expected = -15;
+        int expected = -20; // Wrong expected value
 
         int actual = num1 + num2;
 
