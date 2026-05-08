e1a4c4dd9589f6d32f436417692df4c44c572ccd
Shlokuchavan <gajvakraresedency@gmail.com>
2026-05-08 14:14:51 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -8,32 +8,32 @@ public class AppTest extends TestCase {
         int num1 = 10;
         int num2 = 20;
 
-        int expected = 30; // Correct expected value
+        int expected = 25; // Wrong expected value (intentional failure)
 
         int actual = num1 + num2;
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual); // This test will fail
     }
 
     public void testAdditionWithZero() {
         int num1 = 15;
         int num2 = 0;
 
-        int expected = 15; // Correct expected value
+        int expected = 10; // Wrong expected value (intentional failure)
 
         int actual = num1 + num2;
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual); // This test will fail
     }
 
     public void testNegativeNumbers() {
         int num1 = -5;
         int num2 = -10;
 
-        int expected = -5; // Correct expected value
+        int expected = -20; // Wrong expected value (intentional failure)
 
         int actual = num1 + num2;
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual); // This test will fail
     }
 }
